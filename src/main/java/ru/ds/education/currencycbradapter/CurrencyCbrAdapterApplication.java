7734e28037184fec8135d3a0c52f97b2d1ccd8de
INegmetov <negmetov.i@gmail.com>
2021-04-17 14:33:40 +0400

    init commit

---
+++ src/main/java/ru/ds/education/currencycbradapter/CurrencyCbrAdapterApplication.java
@@ -0,0 +1,13 @@
+package ru.ds.education.currencycbradapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCbrAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyCbrAdapterApplication.class, args);
+	}
+
+}
